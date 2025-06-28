d05446983dc144c76ff3a6260f422d64675778fc
maruf010 <mhbijoy013@gmail.com>
2025-06-28 13:21:05 +0600

    riders all added

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const admin = require("firebase-admin");
+
+dotenv.config();
 
 const stripe = require('stripe')(process.env.PAYMENT_GATEWAY_KEY);
 
+const app = express();
+const port = process.env.PORT || 5000;
 
+
+//middleware
 app.use(cors());
 app.use(express.json());
+
+
+const serviceAccount = require("./firebase-admit-key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vnbrepr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,6 +43,94 @@ async function run() {
 
         const parcelsCollection = client.db("parcelDB").collection("parcels");
         const paymentsCollection = client.db("parcelDB").collection('payments');
+        const usersCollection = client.db("parcelDB").collection('users');
+        const ridersCollection = client.db("parcelDB").collection('riders');
+
+
+
+        //custom middleware jwt
+        const verifyFBToken = async (req, res, next) => {
+            // console.log('Headers in Middleware', req.headers);
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+
+            const token = authHeader.split(' ')[1];
+            if (!token) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+
+            //verify the token
+            try {
+                const decoded = await admin.auth().verifyIdToken(token);
+                req.decoded = decoded;
+                next();
+            }
+            catch (error) {
+                return res.status(403).send({ message: 'forbidden access' })
+            };
+        };
+
+
+        app.post('/users', async (req, res) => {
+            const email = req.body.email;
+
+            const userExists = await usersCollection.findOne({ email });
+            if (userExists) {
+                //update last login if user already exists
+                // await usersCollection.updateOne({ email },
+                //     {
+                //         $set: { last_log_in: new Date().toISOString() }
+                //     }
+                // );
+                return res.status(200).send({ message: 'User already exists', inserted: false })
+            };
+
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
+        //be a rider
+        app.post('/riders', async (req, res) => {
+            const rider = req.body;
+            const result = await ridersCollection.insertOne(rider);
+            res.send(result)
+        });
+
+        app.get('/riders/pending', async (req, res) => {
+            try {
+                const pendingRiders = await ridersCollection.find({ status: 'pending' }).toArray();
+                res.send(pendingRiders);
+            } catch (error) {
+                console.error('Error fetching pending riders:', error);
+                res.status(500).send({ error: 'Internal server error' });
+            }
+        });
+        app.patch("/riders/:id/status", async (req, res) => {
+            const { id } = req.params;
+            const { status } = req.body;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: { status }
+            }
+
+            try {
+                const result = await ridersCollection.updateOne(query, updateDoc);
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ message: "Failed to update rider status" });
+            }
+        });
+        app.get("/riders/active", async (req, res) => {
+            const result = await ridersCollection.find({ status: "active" }).toArray();
+            res.send(result);
+        });
+
+
 
         // POST: Create a new parcel
         app.post('/parcels', async (req, res) => {
@@ -38,9 +138,8 @@ async function run() {
             const result = await parcelsCollection.insertOne(newParcel);
             res.send(result)
         });
-
         // GET: All parcels
-        app.get('/parcels', async (req, res) => {
+        app.get('/parcels', verifyFBToken, async (req, res) => {
             try {
                 const userEmail = req.query.email;
 
@@ -57,7 +156,6 @@ async function run() {
                 res.status(500).send({ message: 'Failed to get parcels' });
             }
         });
-
         //get Id
         app.get('/parcels/:id', async (req, res) => {
             try {
@@ -88,9 +186,18 @@ async function run() {
         });
 
 
-        app.get('/payments', async (req, res) => {
+
+        app.get('/payments', verifyFBToken, async (req, res) => {
+            console.log(req.headers);
+
             try {
                 const userEmail = req.query.email;
+
+                console.log('decoded', req.decoded);
+
+                if (req.decoded.email !== userEmail) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
 
                 const query = userEmail ? { email: userEmail } : {};
                 const options = { sort: { paid_at: -1 } }; // Latest first
@@ -102,23 +209,6 @@ async function run() {
                 res.status(500).send({ message: 'Failed to get payments' });
             }
         });
-
-        app.post("/tracking", async (req, res) => {
-            const { tracking_id, parcel_id, status, message, updated_by = '' } = req.body;
-
-            const log = {
-                tracking_id,
-                parcel_id: parcel_id ? new ObjectId(parcel_id) : undefined,
-                status,
-                message,
-                time: new Date(),
-                updated_by,
-            };
-
-            const result = await trackingCollection.insertOne(log);
-            res.send({ success: true, insertedId: result.insertedId });
-        });
-
         // POST: Record payment and update parcel status
         app.post('/payments', async (req, res) => {
             try {
@@ -162,11 +252,7 @@ async function run() {
             }
         });
 
-
-
-
-
-        //payment
+        //payment Intent
         app.post('/create-payment-intent', async (req, res) => {
             const amountInCents = req.body.amountInCents;
             try {
@@ -181,6 +267,29 @@ async function run() {
                 res.status(500).json({ error: error.message })
             }
         });
+
+
+
+        app.post("/tracking", async (req, res) => {
+            const { tracking_id, parcel_id, status, message, updated_by = '' } = req.body;
+
+            const log = {
+                tracking_id,
+                parcel_id: parcel_id ? new ObjectId(parcel_id) : undefined,
+                status,
+                message,
+                time: new Date(),
+                updated_by,
+            };
+
+            const result = await trackingCollection.insertOne(log);
+            res.send({ success: true, insertedId: result.insertedId });
+        });
+
+
+
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
